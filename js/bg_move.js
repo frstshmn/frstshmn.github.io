61dbc4f03d17e67126feda43d408bb3dfdfb60fc
KorenhaV <korenha.v@gmail.com>
2021-04-20 01:08:32 +0300

    Remade hero block

--- js/bg_move.js
+++ js/bg_move.js
@@ -1,4 +1,5 @@
 $(".background-hero").on("mousemove", function(e) {
-    $(".background-hero").css({ '--x': -e.screenX / 100 + "px", '--y': -e.screenY / 100 + "px" });
-    $(".background-hero-element").css('transform', "translate(" + e.screenX / 200 + "px, " + e.screenY / 100 + "px)");
+    $(".background-hero").css({'--y': -e.screenY / 100 + "px" });
+    $(".background-hero").css({'--x': -e.screenX / 100 + "px" });
+    $(".background-hero-element").css('transform', "translate(" + (e.screenX-50) / 100 + "px, " + (e.screenY-50) / 100 + "px)");
 });
--- js/flies.js
+++ js/flies.js
@@ -1,13 +1,10 @@
-// function move(elem){
-    
-// }
-
 $(document).ready(function() {
     setInterval(() => $(".fly").each(
         function(){
             $( this ).css({
                 'transform': "translate(" + Math.floor((Math.random() * 51) - 25) + "px, "+ Math.floor((Math.random() * 51) - 25) + "px)",
-                'transition': "3s cubic-bezier("+Math.random()+","+Math.random()+","+Math.random()+","+Math.random()+")"
+                'transition': "3s cubic-bezier("+Math.random()+","+Math.random()+","+Math.random()+","+Math.random()+")",
+                'filter': "blur("+Math.floor((Math.random() * 5) - 1)+"px)"
             })
         })
     , 3000);
