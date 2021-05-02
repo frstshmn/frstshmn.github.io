26f28fd44868f6bc3810fda65a2b828687fbfd78
KorenhaV <korenha.v@gmail.com>
2021-05-02 23:44:14 +0300

    Added logos to portfolio

--- js/bg_move.js
+++ js/bg_move.js
@@ -3,3 +3,15 @@ $(".background-hero").on("mousemove", function(e) {
     $(".background-hero").css({'--x': -e.screenX / 100 + "px" });
     $(".background-hero-element").css('transform', "translate(" + (e.screenX-50) / 100 + "px, " + (e.screenY-50) / 100 + "px)");
 });
+
+$(".portfolio-logo").on("mousemove", function(e) {
+    var pos = $(this).offset();
+    var elem_left = pos.left;
+    var elem_top = pos.top;
+    // положение курсора внутри элемента
+    var ww = e.pageX - elem_left;
+    var wh = e.pageY - elem_top;
+
+    $(this).css('--mouseX', -ww / 20 + "deg");
+    $(this).css('--mouseY', -wh / 20 + "deg");
+});
--- js/nav.js
+++ js/nav.js
@@ -1,55 +1,8 @@
-// function showWeb(){
-//     $("#main_screen").hide("slide", { direction: "right" }, 500);
-//     $("#web_portfolio").show("slide", { direction: "right" }, 500);
-// }
-
-// function hideWeb(){
-
-// }
-
-// function showGraphics(){
-
-// }
-
-// function hideGraphics(){
-
-// }
-
-// $(document).ready(function() {
-//     $("#web_portfolio").hide();
-//     $("#main_screen").show();
-
-//     $('#web_portfolio_button').click(() => {
-
-//         $("#web_portfolio").toggle("slide", { direction: "right" }, 500);
-//         //$("#main_screen").toggle("slide", { direction: "right" }, 500);
-//     });
-// });
-
-function toggleWeb() {
-    // Hide to left / show from left
-    $("#main_screen").toggle("slide", { direction: "right" }, 500);
-
-    // Show from right / hide to right
-    $("#web_portfolio").toggle("slide", { direction: "left" }, 500);
-}
-
-$(document).ready(function() {
-    //$('#exampleModal3').modal('toggle');
-    //$("#main_screen").css({ 'filter': "blur(5px)" });
-    $("#web_portfolio").toggle("slide", { direction: "left" }, 500);
-    $('#web_portfolio_button').click(toggleWeb);
-    $('#web_portfolio_button_back').click(toggleWeb);
+$('#portfolioTabs button').click(function (e) {
+    $('#portfolioTabs button').removeClass('pressed');
+    $(this).addClass('pressed');
+    $('#portfolioTabContent>div').removeClass('active');
+    $('#portfolioTabContent>div').removeClass('show');
+    $($(this).data('bsTarget')).addClass('active');
+    $($(this).data('bsTarget')).addClass('show');
 });
-
-// $("#exampleModal3").on("hidden.bs.modal", function() {
-//     $("#main_screen").css({'filter': "blur(0px)"});
-// })
-
-function copyText(string) {
-    navigator.clipboard.writeText("<empty clipboard>").then(function() {
-        /* clipboard successfully set */
-    }, function() {
-        /* clipboard write failed */
-    });
-}
